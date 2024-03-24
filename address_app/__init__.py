ebdf16598a12a31bf2cf929bc379e7500d364698
Michael Levin <michael.levi.fx@gmail.com>
2024-03-25 01:38:20 +0200

    style: replace lines

--- address_app/__init__.py
+++ address_app/__init__.py
@@ -1,11 +1,12 @@
 __app_name__ = "address_app"
 __version__ = "0.2.3"
+__all__ = ["AdbDatabase", "__app_name__", "__version__"]
 
 # initialize logger
 from .base import get_logger, consts, exceptions
 
-__all__ = ["AdbDatabase", "__app_name__", "__version__"]
 
 from .database import AdbDatabase
+from .model import AddressBook
 from .view import ViewManager
 from .serialize import SerializationManager
