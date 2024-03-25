96c21e9c3675d7a1e754dab4eef37833fe4bbd63
Michael Levin <michael.levi.fx@gmail.com>
2024-03-25 02:47:54 +0200

    new test

--- tests/test_adb_base.py
+++ tests/test_adb_base.py
@@ -8,6 +8,27 @@ class TestAdbDatabaseBase(unittest.TestCase):
         self.root = "tests"
         self.adb = app.AdbDatabase(self.root)
 
+    def test_init(self):
+        self.assertIsNotNone(
+            app.AdbDatabase(),
+            "AdbDatabase instance should be created without a root path",
+        )
+
+        self.assertIsNotNone(
+            app.AdbDatabase("tests"),
+            "AdbDatabase instance should be created with a root path",
+        )
+
+        self.assertIsNotNone(
+            app.AdbDatabase(123),
+            "AdbDatabase instance should be created with a root path",
+        )
+
+        self.assertIsNotNone(
+            app.AdbDatabase(None),
+            "AdbDatabase instance should be created with a root path",
+        )
+
     def test_singleton(self):
         """Test that the AdbDatabase is a singleton."""
         same_path = os.path.abspath(self.root)
