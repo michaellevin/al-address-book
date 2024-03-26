35fe06e22283ef85313ab0e7192e8d5e2e952761
Michael Levin <michael.levi.fx@gmail.com>
2024-03-26 23:47:08 +0200

    add full storage path

--- address_app/base/consts.py
+++ address_app/base/consts.py
@@ -1,7 +1,14 @@
 import re
+import tempfile
+
+#: Default root path
+DEFAULT_ROOT_PATH = tempfile.gettempdir()
 
 #: Relative path to the storage file for database
 RELATIVE_STORAGE_PATH = "adb/adb.pickle"
+
+#: Default storage file path
+DEFAULT_STORAGE_FULL_PATH = f"{DEFAULT_ROOT_PATH}/{RELATIVE_STORAGE_PATH}"
 
 #: Validation regex for phone number
 VALIDATE_PHONE_NO_REGEX = re.compile(r"^[0-9()\+\-\s]+$")
