d3b98e73023a52b231a6ef679827488690f257ef
Michael Levin <michael.levi.fx@gmail.com>
2024-03-28 08:40:47 +0200

    fix

--- address_app/base/validator.py
+++ address_app/base/validator.py
@@ -12,16 +12,21 @@ from .exceptions import (
 class ContactValidation:
     @staticmethod
     def validate_name(name: str):
+        """Validate the name of a contact: not empty."""
         if not isinstance(name, str) or not name.strip() or name.isdigit():
             raise InvalidContactNameException(name)
 
     @staticmethod
     def validate_address(address: str):
+        """Validate the address of a contact: not empty."""
         if not isinstance(address, str) or not address.strip():
             raise InvalidContactAddressException(address)
 
     @staticmethod
     def validate_phone_no(phone_no: Optional[str]):
+        """Validate the phone number of a contact.
+        It must include only digits, parentheses, plus sign, hyphen, space, or be empty.
+        """
         if phone_no is not None and not re.match(VALIDATE_PHONE_NO_REGEX, phone_no):
             raise InvalidContactPhoneNumberException(phone_no)
 
